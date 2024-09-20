a64dd5c55efc46edeb0136812af25e3446082322
Nico <nicoherrera0831@gmail.com>
2024-09-19 22:59:18 -0500

    debug

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,9 @@ import { getFantasyData } from '$lib/fantasyDataService';
 
 export async function load() {
 	try {
+		console.log('Loading');
 		const weeklyData = await getFantasyData();
+		console.log('Loading weekly');
 		return { weeklyData };
 	} catch (error) {
 		console.error('Error loading fantasy data:', error);
