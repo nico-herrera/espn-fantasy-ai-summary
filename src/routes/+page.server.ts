20f6449decec19f67f8dbb1143998da1aa7b11b0
Nico <nicoherrera0831@gmail.com>
2024-09-19 23:02:14 -0500

    remove consoles

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,9 +2,7 @@ import { getFantasyData } from '$lib/fantasyDataService';
 
 export async function load() {
 	try {
-		console.log('Loading');
 		const weeklyData = await getFantasyData();
-		console.log('Loading weekly');
 		return { weeklyData };
 	} catch (error) {
 		console.error('Error loading fantasy data:', error);
